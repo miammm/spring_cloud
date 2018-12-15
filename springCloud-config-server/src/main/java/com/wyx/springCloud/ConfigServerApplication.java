3ed34d04e99455ebf95cf4105f80f34ddaaceb7c
wyx <936741678@qq.com>
2018-12-16 00:48:42 +0800

    配置中心--配置文件

--- springCloud-config-server/src/main/java/com/wyx/springCloud/ConfigServerApplication.java
+++ springCloud-config-server/src/main/java/com/wyx/springCloud/ConfigServerApplication.java
@@ -2,10 +2,11 @@ package com.wyx.springCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
